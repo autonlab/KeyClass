4f9566db57a36b1c81ddd4cf2fd46fa808cc4aac
Mononito Goswami <mgoswami@gpu22.int.autonlab.org>
2022-04-12 17:32:00 -0400

    encode dataset works

--- scripts/encode_datasets.py
+++ scripts/encode_datasets.py
@@ -9,22 +9,13 @@ import torch
 import pickle
 from config import Parser
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--data_path', default='/zfsauton/project/public/chufang/classes/',
-#                     help='Dataset directory')
-# parser.add_argument('--dataset', default='imdb',
-#                     help='Dataset')
-# parser.add_argument('--model_name', default='all-mpnet-base-v2',
-#                     help='Sentence Encoder model to use')
-# args = parser.parse_args()
-
-parser = Parser()
+parser = Parser(config_file_path='../default_config.yml')
 args = parser.parse()
 
-model = Encoder(model_name=args.base_encoder, device='cuda' if torch.cuda.is_available() else 'cpu')
+model = Encoder(model_name=args['base_encoder'], device='cuda' if torch.cuda.is_available() else 'cpu')
 
 for split in ['train', 'test']:
-    text = fetch_data(dataset=args.dataset, split=split, path=args.data_path)
+    text = fetch_data(dataset=args['dataset'], split=split, path=args['data_path'])
     embeddings = model.get_embeddings(text=text, batch_size=128)
-    with open(join(args.data_path, args.dataset, f'{split}_embeddings.pkl'), 'wb') as f:
+    with open(join(args['data_path'], args['dataset'], f'{split}_embeddings.pkl'), 'wb') as f:
         pickle.dump(embeddings, f)
--- scripts/label_data.py
+++ scripts/label_data.py
@@ -10,21 +10,21 @@ import torch
 from os.path import join
 from config import Parser
 
-parser = Parser()
+parser = Parser(config_file_path='../default_config.yml')
 args = parser.parse()
 
 # Load training data
-train_text = utils.fetch_data(dataset=args.dataset, path=args.data_path, split='train')
+train_text = utils.fetch_data(dataset=args['dataset'], path=args['data_path'], split='train')
 
-with open(join(args.data_path, args.dataset, 'train_labels.txt'), 'rb') as f:
+with open(join(args['data_path'], args['dataset'], 'train_labels.txt'), 'rb') as f:
     y_train = f.readlines()
 y_train = np.array([int(i.replace('\n','')) for i in y_train])
 
-with open(join(args.data_path, args.dataset, 'train_embeddings.txt'), 'rb') as f:
+with open(join(args['data_path'], args['dataset'], 'train_embeddings.txt'), 'rb') as f:
     X_train = pickle.load(f)
 
 # Print dataset statistics
-print(f'Getting labels for the {args.dataset} data...')
+print(f"Getting labels for the {args['dataset']} data...")
 print(f'Size of the data: {len(train_text)}')
 print('Class distribution', np.unique(y_train, return_counts=True))
 
@@ -40,10 +40,10 @@ proba_preds = labeler.get_labels(text_corpus=train_text, label_names=label_names
                                  label_model_lr=args.label_model_lr, label_model_n_epochs=args.label_model_n_epochs, 
                                  verbose=True)
 
-y_train_pred = np.argmax(proba_preds, axis=1...)
+y_train_pred = np.argmax(proba_preds, axis=1)
 
 print('Label Model Training Accuracy', np.mean(y_train_pred==y_train))
 print('Label Model Predictions: Unique and Counts', np.unique(y_train_pred, return_counts=True)
-if not os.path.exists(args.save_results_path):   os.makedirs(args.save_results_path)
-pickle.dump(proba_preds, open(join(save_results_path, 'proba_preds.pkl'), 'wb'))
-    
+if not os.path.exists(args['save_results_path']): os.makedirs(args['save_results_path'])
+with open(join(args['save_results_path'], 'proba_preds.pkl'), 'wb')) as f:
+    pickle.dump(proba_preds, f)
