745c96b8b2d367d19d4795bb25c0ba877f0765ca
Mononito Goswami <mononitog@hotmail.com>
2022-05-10 14:39:30 -0400

    Update train_model_from_scratch.sh

--- scripts/train_model_from_scratch.sh
+++ scripts/train_model_from_scratch.sh
@@ -1,17 +1,21 @@
 # Label all the datasets
 
-CONFIG="../config_coronary_angio_cabg.yml" # Add path to your config file
+# CONFIG="/zfsauton2/home/mgoswami/KeyClass/config_coronary_angio_cabg.yaml" # Add path to your config file
+CONFIG="../config_coronary_angio_cabg.yaml" # Add path to your config file
  
 export CUDA_VISIBLE_DEVICES=3 # Comment if you want to run on all the GPUs. 
 
-echo "======================="
+echo "\n======================="
 echo "Encoding the dataset..."
 python encode_datasets.py --config=$CONFIG
-
 echo "======================="
+
+echo "\n======================="
 echo "Labeling the data..."
 python label_data.py --config=$CONFIG
-
 echo "======================="
+
+echo "\n======================="
 echo "Training the downstream classifier..."
 python train_downstream_model.py --config=$CONFIG
+echo "======================="
