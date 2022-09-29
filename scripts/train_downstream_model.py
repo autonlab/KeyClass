b2c34e7dedaccfac3884e637e8b9fe096aa5063b
Mononito Goswami <mononitog@hotmail.com>
2022-09-29 14:42:08 -0400

    yapf formatting

--- scripts/train_downstream_model.py
+++ scripts/train_downstream_model.py
@@ -1,3 +1,26 @@
+# coding=utf-8
+# MIT License
+
+# Copyright (c) 2020 Carnegie Mellon University, Auton Lab
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import sys
 
 sys.path.append('../keyclass/')
@@ -313,12 +336,3 @@ def test(args_cmd, end_model_path, end_model_self_trained_path):
         n_jobs=args['n_jobs'])
     print('testing_metrics after self train', testing_metrics)
     return testing_metrics
-
-
-# if __name__ == "__main__":
-#     parser_cmd = argparse.ArgumentParser()
-#     parser_cmd.add_argument('--config', default='../default_config.yml', help='Configuration file')
-#     parser_cmd.add_argument('--random_seed',default=0,help="Random Seed")
-#     args_cmd = parser_cmd.parse_args()
-
-#     run(args_cmd)
